468397066517d0cb55e5d93ed65f82c4bb9d7c53
kevin hu <sky790312yks@gmail.com>
2016-04-20 17:52:39 +0800

    completed basic function

--- js/test.js
+++ js/test.js
@@ -1,7 +1,13 @@
 "use strict";
 
 
+/*
 
+	padding -
+	1. drop
+	2. random move
+
+ */
 
 
 
@@ -9,6 +15,21 @@
 	var methodObj = {
 		setting: {
 			count: 12
+		},
+		utils: {
+			hasClass: function(element, checkClass) {
+				return (' ' + element.className + ' ').indexOf(' ' + checkClass + ' ') > -1;
+			},
+			findCoord: function(x, y) {
+				var imgArray = document.querySelectorAll('.img');
+
+				for(var i = 0, j = imgArray.length; i < j; i += 1) {
+						// find this (x.y) img
+						if(x === imgArray[i].dataset.coorx && y === imgArray[i].dataset.coory) {
+							return imgArray[i];
+						}
+				}
+			}
 		},
 		// counter
 		counter: function() {
@@ -20,7 +41,7 @@
 				methodObj.setting.count--;
 				countdownid = setTimeout(function () {
 					methodObj.createCoord(function(){
-						// show img
+
 					});
 					// methodObj.append();
 	        methodObj.counter();
@@ -29,20 +50,28 @@
 		},
 		// random create coordinate
 		createCoord: function(callback) {
-			var coordX = Math.floor(Math.random() * 4);
-			var coordY = Math.floor(Math.random() * 3);
-			if(methodObj.checkCoord(coordX, coordY)) {
+			var coordX = Math.floor(Math.random() * 4).toString();
+			var coordY = Math.floor(Math.random() * 3).toString();
+			var thisEle = methodObj.utils.findCoord(coordX, coordY);
+
+			if(methodObj.utils.hasClass(thisEle, 'show')) {
 				methodObj.createCoord();
 				return;
 			}
-      // if (typeof callback === 'function') {
-      //   callback();
-      // }
+
+			thisEle.className += ' show img' + methodObj.setting.count;
+
+      if (typeof callback === 'function') {
+        callback();
+      }
 		},
 		// check coordinate exist or not
-		checkCoord: function(x, y) {
-			// check show class
-		},
+		// checkCoord: function(x, y) {
+		// 	// check show class
+		// 	var thisEle = methodObj.utils.findCoord(x, y);
+
+		// 	return (methodObj.utils.hasClass(thisEle, 'show')) ? false : true;
+		// },
 		// append: function() {
 		// 	var target = document.querySelector(".section");
 		// 	var str = '<div class="img"> \
